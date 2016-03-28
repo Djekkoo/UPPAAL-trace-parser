fd31197e78def2514a72e435b67411c20e046f2b
djekkoo <djekkoo@hotmail.com>
2016-03-28 17:11:45 +0200

    basic project set-up

---
+++ src/parser/Main.java
@@ -0,0 +1,11 @@
+package parser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Working project set-up!");
+		return;
+	}
+
+}
